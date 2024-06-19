8edadd0e508a7df08ce9b8faa8d5595ef5902675
glaboy02 <glaboy02@gmail.com>
2024-06-19 00:26:07 -0400

    Added to the index.html, style.css and app.js

--- src/app.js
+++ src/app.js
@@ -15,12 +15,45 @@ document
 document
     .getElementById("bookmark")
     .addEventListener("click", () => navigate("bookmarkView"));
+
 document
-    .getElementById("about")
-    .addEventListener("click", () => navigate("aboutView"));
+    .getElementById("account")
+    .addEventListener("click", () => {
+        const info = document.querySelector('.info');
+
+        if(getComputedStyle(info).textDecorationLine !== 'line-through'){
+            navigate("accountView")
+        }
+    });
+
 document
-    .getElementById("anime")
-    .addEventListener("click", () => navigate("animeView"));
+    .getElementById("plus")
+    .addEventListener("click", () => {
+        const info = document.querySelector('.info');
+        return info.removeAttribute('style');
+    });
+
+document
+    .getElementById("minus")
+    .addEventListener("click", () => {
+        const info = document.querySelector('.info');
+        return info.setAttribute(
+            'style',
+            'text-decoration: line-through'
+        );
+    });
+
+    
+document
+    .getElementById("signInGo")
+    .addEventListener("click", () => {
+        console.log('Go Button clicked')
+        navigate("accountView")
+    });
+
+document
+    .getElementById("signUpGo")
+    .addEventListener("click", () => navigate("accountView"));
 
 // Initialize with the home view
 navigate("homeView");
@@ -33,4 +66,6 @@ document.querySelectorAll(".image-container img").forEach((img) => {
     parent.insertBefore(this, parent.firstChild); // Move the clicked image to the beginning
     });
 });
+
 });
+
